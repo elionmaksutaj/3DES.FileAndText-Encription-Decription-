9722451cd8afa20d5d5d1004357ab9ca14b44e63
elisagashi <elisagashi99@gmail.com>
2024-04-21 12:22:10 +0300

    Update text.py

--- text.py
+++ text.py
@@ -3,41 +3,42 @@ from Crypto.Random import get_random_bytes
 import binascii
 
 while True:
-     try:
-       key = DES3.adjust_key_parity(get_random_bytes(24))
+    try:
+        key = DES3.adjust_key_parity(get_random_bytes(24))
         break
     except ValueError:
-        print("Çelësi i gjeneruar ka barazi të gabuar. Gjenerimi i një çelësi të ri...")
+        print("Generated key has incorrect parity. Generating a new key...")
 
 def encrypt(msg):
-     cipher = DES3.new(key, DES3.MODE_EAX)
-     nonce = cipher.nonce
-     ciphertext = cipher.encrypt(msg.encode('ascii'))
-     return nonce, ciphertext
+    cipher = DES3.new(key, DES3.MODE_EAX)
+    nonce = cipher.nonce
+    ciphertext = cipher.encrypt(msg.encode('ascii'))
+    return nonce, ciphertext
 
 def decrypt(nonce, ciphertext):
     cipher = DES3.new(key, DES3.MODE_EAX, nonce=nonce)
     plaintext = cipher.decrypt(ciphertext)
-     return plaintext.decode('ascii')
+    return plaintext.decode('ascii')
 
 while True:
-    print("Zgjedhe një opsion:")
-    print("1. Enkriptim")
-    print("2. Dekriptim")
-    choice = input("Shtyp zgjedhjen tuaj (1 or 2): ")
+    print("Choose an option:")
+    print("1. Encrypt")
+    print("2. Decrypt")
+    choice = input("Enter your choice (1 or 2): ")
+
     if choice == '1':
-        plaintext = input('Shtyp plaintext: ')
+        plaintext = input('Enter plaintext: ')
         nonce, ciphertext = encrypt(plaintext)
         print('Nonce:', binascii.hexlify(nonce).decode('ascii'))
         print('Cipher text:', ciphertext.hex())
     elif choice == '2':
-        ciphertext = bytes.fromhex(input('Shtyp ciphertext në formatin  hex: '))
-        nonce = binascii.unhexlify(input('Shtyp nonce në formatin hex: '))
+        ciphertext = bytes.fromhex(input('Enter ciphertext in hex format: '))
+        nonce = binascii.unhexlify(input('Enter nonce in hex format: '))
         plaintext = decrypt(nonce, ciphertext)
         print('Decrypted text:', plaintext)
-     else:
-        print("Zgjedhje e pasaktë. Të lutem shtyp 1 ose 2.")
+    else:
+        print("Invalid choice. Please enter 1 or 2.")
 
-        again = input("Dëshiron të performon një operacion tjetër? (po/jo): ")
-    if again.lower() != 'po':
+    again = input("Do you want to perform another operation? (yes/no): ")
+    if again.lower() != 'yes':
         break
