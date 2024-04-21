a459e0e49eebe66860fc488f7c50621e6ace9805
elionmaksutaj <150678735+elionmaksutaj@users.noreply.github.com>
2024-04-21 11:31:04 +0200

    Update File.py

--- File.py
+++ File.py
@@ -18,6 +18,21 @@ while True:
     
     
     cipher = DES3.new(tdes_key, DES3.MODE_EAX, nonce=b'0')
+    with open(file_path, 'rb') as input_file:
+        file_bytes = input_file.read()
+        
+        if operation == '1':
+            # Perform Encryption operation
+            new_file_bytes = cipher.encrypt(file_bytes)
+        else:
+            # Perform Decryption operation
+            new_file_bytes = cipher.decrypt(file_bytes)
+    
+    
+    with open(file_path, 'wb') as output_file:
+        output_file.write(new_file_bytes)
+        print('Operation Done!')
+        break
 
     
     
